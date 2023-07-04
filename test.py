77825179c32628cc65814facdd3b4170048b3128
counterany <116473561+counterany@users.noreply.github.com>
2023-07-04 17:00:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -30,19 +30,7 @@ def read_load_image2(file_path):
         img = load_image(img)
         all_img.append(img)
     return all_img, shape_m
-
-
-def save_final_images(file_path, result2):
-    """
-    保存增强后的图片
-    :param file_path: 保存的文件地址
-    :param result2: 增强后的图片矩阵
-    :return: 无
-    """
-    result2 = result2.cpu().detach().numpy()  # 将结果转化为numpy类型，用于后续保存图片
-    result2 = np.squeeze(result2)
-    cv2.imwrite(file_path, result2 * 255.0)
-
+    
 
 save_dir = "G:\\my_baby\\retinex\\decom_train\\last"
 if not os.path.exists(save_dir):
@@ -90,7 +78,7 @@ for i in range(0, eval_len):
     eval_image1 = eval_image1.detach().numpy()
     eval_image1 = cv2.resize(eval_image1, new_shape)
     # 保存两张图片
-    save_image(os.path.join("G:\\my_baby\\retinex\\adjust_train\\retinex", f"__{i}.png"), eval_image1, image)
+    save_image(os.path.join(".\\adjust_train\\retinex", f"__{i}.png"), eval_image1, image)
     # 对分解网络获得的反射图和光照图进行处理
     # 对光照图进行处理
     eval_I = torch.cat((eval_I, eval_I, eval_I), dim=1)
@@ -105,40 +93,3 @@ for i in range(0, eval_len):
     eval_R = cv2.resize(eval_R, new_shape)
     # 保存图片
     save_image(os.path.join(save_dir, f"__{i}.png"), eval_R, image)
-'''
-# 选择第几张照片进行测试
-i = 12
-eval_image1 = eval_image[i]
-eval_image1 = torch.tensor(eval_image1).cuda()
-eval_image1 = eval_image1.reshape(1, eval_image1.shape[0], eval_image1.shape[1], eval_image1.shape[2])  # 将图片矩阵重构大小输入网络
-eval_image1 = eval_image1.permute([0, 3, 1, 2])  # cv2读入的图片的channel放在了最后，把它调到第二
-eval_R, eval_I = g_model(eval_image1)  # 获得反射图和光照图
-eval_R = eval_R.cpu()
-eval_I = eval_I.cpu()
-eval_II = a_model(eval_I, eval_R)  # 调整光照图
-eval_II = torch.cat((eval_II, eval_II, eval_II), dim=1)  # 将光照图扩充为3个通道
-image = eval_R * eval_II  # 获得重构后的图片
-image = image.squeeze(0)  # 除去第一维度
-image = image.permute([1, 2, 0])  # 还原图片原始数据维度
-image = image.detach().numpy()  # 还原为数组
-# 还原图片大小并保存
-one_shape = eval_shape[i]
-new_shape = (int(one_shape[1]), int(one_shape[0]))  # 照片的原始维度
-image = cv2.resize(image, new_shape)  # 将照片还原成原始维度
-
-eval_image1 = eval_image1.squeeze(0)
-eval_image1 = eval_image1.permute([1, 2, 0])
-eval_image1 = eval_image1.cpu().detach().numpy()
-eval_image1 = cv2.resize(eval_image1, new_shape)
-save_image(os.path.join("G:\\my_baby\\retinex\\adjust_train", f"__{i}.png"), eval_image1, image)
-eval_I = torch.cat((eval_I, eval_I, eval_I), dim=1)
-eval_I = eval_I.squeeze(0)
-eval_I = eval_I.permute([1, 2, 0])
-eval_I = eval_I.detach().numpy()
-eval_I = cv2.resize(eval_I, new_shape)
-eval_R = eval_R.squeeze(0)
-eval_R = eval_R.permute([1, 2, 0])
-eval_R = eval_R.detach().numpy()
-eval_R = cv2.resize(eval_R, new_shape)
-save_image(os.path.join(save_dir, f"__{i}.png"), eval_R, eval_I)
-'''
